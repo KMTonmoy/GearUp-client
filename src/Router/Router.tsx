5a5e4ca6b26c014cac3008122fe37a55ee20ad17
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-24 20:57:48 +0600

    first commit

---
+++ src/Router/Router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+ 
+        ]
+    },
+    {
+        path: "*",
+        element: <>Error 404 Page Dosent Exits</>,
+    }
+
+]);
