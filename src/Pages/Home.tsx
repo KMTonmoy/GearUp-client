db9e2f87a6e7d9d89b9f0a324a0e6c7484f915b8
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-24 21:01:46 +0600

    Tailwind css added

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home Page
+            <h1 className='text-red-500'>This Is Home Page</h1>
         </div>
     );
 };
