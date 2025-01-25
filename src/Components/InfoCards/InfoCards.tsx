750fde3b881fc3cc02cd5f3883f1816a4595cd1a
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-25 10:59:17 +0600

    Info Card

---
+++ src/Components/InfoCards/InfoCards.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaCaretRight } from "react-icons/fa";
+
+const InfoCards = () => {
+    return (
+        <div className='flex md:flex-row flex-col gap-5 items-center'>
+            <div className="relative w-full sm:w-1/3 h-[300px] bg-cover bg-center" style={{ backgroundImage: "url(https://bikun-store-demo.myshopify.com/cdn/shop/files/bannerV1-img1.jpg?v=1664587573)" }}>
+                <div className="absolute top-1/2 left-35 transform -translate-x-1/2 -translate-y-1/2 text-white text-left">
+                    <h1 className="text-2xl font-semibold mb-2">Singletrack Speed</h1>
+                    <h5 className="text-lg mb-4">The All-New Trance Advanced</h5>
+                    <a href="/shop" className="text-white flex items-center hover:text-orange-500   transition-colors">
+                        View Collection <FaCaretRight className="ml-2" />
+                    </a>
+                </div>
+            </div>
+
+            <div className="relative w-full sm:w-1/3 h-[300px] bg-cover bg-center" style={{ backgroundImage: "url(https://bikun-store-demo.myshopify.com/cdn/shop/files/bannerV1-img2.jpg?v=1664587573)" }}>
+                <div className="absolute top-1/2 left-35 transform -translate-x-1/2 -translate-y-1/2 text-white text-left">
+                    <h1 className="text-2xl font-semibold mb-2">Singletrack Speed</h1>
+                    <h5 className="text-lg mb-4">The All-New Trance Advanced</h5>
+                    <a href="/shop" className="text-white flex items-center hover:text-orange-500   transition-colors">
+                        View Collection <FaCaretRight className="ml-2" />
+                    </a>
+                </div>
+            </div>
+
+            <div className="relative w-full sm:w-1/3 h-[300px] bg-cover bg-center" style={{ backgroundImage: "url(https://bikun-store-demo.myshopify.com/cdn/shop/files/bannerV1-img3.jpg?v=1664587574)" }}>
+                <div className="absolute top-1/2 left-35 transform -translate-x-1/2 -translate-y-1/2 text-white text-left">
+                    <h1 className="text-2xl font-semibold mb-2">Singletrack Speed</h1>
+                    <h5 className="text-lg mb-4">The All-New Trance Advanced</h5>
+                    <a href="/shop" className="text-white flex items-center hover:text-orange-500   transition-colors">
+                        View Collection <FaCaretRight className="ml-2" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InfoCards;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Banner from '../Components/Banner/Banner';
+import InfoCards from '../Components/InfoCards/InfoCards';
 
 const Home = () => {
     return (
         <>
-            <div>
+            <div className='mb-10'>
                 <Banner />
             </div>
-            <div className='max-w-7xl min-h-[100vh] mx-auto '>
-
+            <div className='max-w-7xl min-h-[100vh] mx-auto flex flex-col gap-10 '>
+                <InfoCards />
             </div>
         </>
     );
